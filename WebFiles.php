dbb6b08e97856b3db63df8ea61072b98d43f6e78
Eduard Kracmar <kracmar@dannax.sk>
2011-12-19 16:48:49 +0100

    reformatting

--- WebFiles.php
+++ WebFiles.php
@@ -7,36 +7,39 @@ namespace WebLoader;
  *
  * @author kraken
  */
-class WebFiles {
+class WebFiles
+{
 
     private $files = array(), $linked = array();
 
 
     /**
      * Add file and its depentents
-     * @param string $name
-     * @param null|string $path
+     * @param string            $name
+     * @param null|string       $path
      * @param null|string|array $depends
      */
-    public function addFile($name, $path = null, $depends = null) {
-	if (\key_exists($name, $this->files)) {
-	    throw new \LogicException("File '$name' already exists");
-	}
+    public function addFile($name, $path = null, $depends = null)
+    {
+        if (\key_exists($name, $this->files)) {
+            throw new \LogicException("File '$name' already exists");
+        }
 
-	if (\is_string($depends)) {
-	    $depends = array($depends);
-	}
-	elseif(\is_null($depends)) {
-	    $depends = array();
-	}
+        if (\is_string($depends)) {
+            $depends = array($depends);
+        }
+        elseif (\is_null($depends)) {
+            $depends = array();
+        }
 
-	foreach ($depends as $dependent) {
-	    if (!\key_exists($dependent, $this->files)) {
-		throw new \LogicException("File '$name' depends on unknown file '$dependent'");
-	    }
-	}
-	$this->files[$name] = (object) array('deps' => $depends, 'path' => $path);
-	return $this;
+        foreach ($depends as $dependent) {
+            if (!\key_exists($dependent, $this->files)) {
+                throw new \LogicException("File '$name' depends on unknown file '$dependent'");
+            }
+        }
+        $this->files[$name] = (object)array('deps' => $depends,
+                                            'path' => $path);
+        return $this;
     }
 
 
@@ -63,77 +66,86 @@ class WebFiles {
 	return $this;
     }
 
+/**
+ * Get array of paths for item with $name with dependancies
+ * @param string $name
+ * @return array
+ */
+private
+function getDeps($name)
+{
+    $paths = array();
+    foreach ($this->files[$name]->deps as $dependant) {
+        $_paths = $this->getDeps($dependant);
+        if (count($_paths)) {
+            $paths = \array_merge($paths, $_paths);
+        }
+    }
+    if ($this->files[$name]->path) {
+        $paths[] = $this->files[$name]->path;
+    }
+    return $paths;
+}
 
-    /**
-     * Get array of paths for item with $name with dependancies
-     * @param string $name
-     * @return array
-     */
-    private function getDeps($name) {
-	$paths = array();
-	foreach ($this->files[$name]->deps as $dependant) {
-	    $_paths = $this->getDeps($dependant);
-	    if (count($_paths)) {
-		$paths = \array_merge($paths, $_paths);
-	    }
-	}
-	if ($this->files[$name]->path) {
-	    $paths[] = $this->files[$name]->path;
-	}
-	return $paths;
+
+/**
+ * Get array of paths for all linked files
+ * @return array
+ */
+public
+function getLinkedFiles()
+{
+    $paths = array();
+
+    foreach ($this->linked as $name) {
+        $_paths = $this->getDeps($name);
+        if (count($_paths)) {
+            $paths = \array_merge($paths, $_paths);
+        }
     }
 
+    $paths = \array_unique($paths);
+    return $paths;
+}
 
-    /**
-     * Get array of paths for all linked files
-     * @return array
-     */
-    public function getLinkedFiles() {
-	$paths = array();
 
-	foreach ($this->linked as $name) {
-	    $_paths = $this->getDeps($name);
-	    if (count($_paths)) {
-		$paths = \array_merge($paths, $_paths);
-	    }
-	}
+/**
+ * Get only array of css files for all linked files
+ * @return array
+ */
+public
+function getLinkedCssFiles()
+{
+    return $this->filterFilesByExtension('css');
+}
 
-	$paths = \array_unique($paths);
-	return $paths;
+
+/**
+ * Get only array of js files for all linked files
+ * @return array
+ */
+public
+function getLinkedJsFiles()
+{
+    return $this->filterFilesByExtension('js');
+}
+
+
+/**
+ * Filter array for files with $extension
+ * @param string $extension
+ * @return array
+ */
+private
+function filterFilesByExtension($extension)
+{
+    $paths = $this->getLinkedFiles();
+    foreach ($paths as $key => $path) {
+        if (!\preg_match('#' . $extension . '$#', \strtolower($path))) {
+            unset($paths[$key]);
+        }
     }
-
-
-    /**
-     * Get only array of css files for all linked files
-     * @return array
-     */
-    public function getLinkedCssFiles() {
-	return $this->filterFilesByExtension('css');
-    }
-
-
-    /**
-     * Get only array of js files for all linked files
-     * @return array
-     */
-    public function getLinkedJsFiles() {
-	return $this->filterFilesByExtension('js');
-    }
-
-
-    /**
-     * Filter array for files with $extension
-     * @param string $extension
-     * @return array
-     */
-    private function filterFilesByExtension($extension) {
-	$paths = $this->getLinkedFiles();
-	foreach ($paths as $key => $path) {
-	    if (!\preg_match('#' . $extension . '$#', \strtolower($path))) {
-		unset($paths[$key]);
-	    }
-	}
-	return $paths;
-    }
+    return $paths;
+}
 }
 
